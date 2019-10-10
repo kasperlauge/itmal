1a3fcbf428cc8716a8deabbf02ca5763b6a91bf8
Kasper Lauge Madsen <kasperlaugemadsen@gmail.com>
2019-10-10 18:56:27 +0200

    Added uge4 oevelse 1 file

--- Journal_2/uge4_oevelse1.py
+++ Journal_2/uge4_oevelse1.py
@@ -27,7 +27,7 @@ labels = digits.target
 
 # show data
 plt.figure(0)
-plt.imshow(np.array(digits.data[4]).reshape(8,8), cmap='gray')
+plt.imshow(np.array(digits.data[0]).reshape(8,8), cmap='gray')
 
 plt.figure(1)
 plt.imshow(np.array(digits.data[9]).reshape(8,8), cmap='gray')
@@ -37,6 +37,23 @@ data_2_dim = pca.fit(data.T)
 
 components = data_2_dim.components_
 
-plt.figure(2)
-plt.scatter(data_2_dim.components_[0,:], data_2_dim.components_[1,:], c=labels, cmap='viridis')
+#plt.figure(2)
+#plt.scatter(data_2_dim.components_[0,:], data_2_dim.components_[1,:], c=labels, cmap='viridis')
+#plt.legend(labels)
+#plt.show()
+
+colors = ['red', 'blue', 'green', 'cyan', 'orange', 'purple', 'black', 'grey', 'pink', 'yellow']
+
+fig, ax = plt.subplots()
+for number in range(10):
+    numArr = np.where(labels == number)
+    x = data_2_dim.components_[0,numArr[0]]
+    y = data_2_dim.components_[1,numArr[0]]
+    ax.scatter(x, y, c=colors[number], label=number,
+               alpha=0.3, edgecolors='none')
+
+ax.legend()
+ax.set_title('PCA transform of "Handwritten digits"')
+ax.grid(True)
+
 plt.show()
